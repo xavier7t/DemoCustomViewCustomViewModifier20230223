f055e37ba740985a32c5bca4c66d7fd51887dd36
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-23 23:34:01 -0500

    DemoCustomViewCustomViewModifier20230223

--- DemoCustomViewCustomViewModifier20230223/ContentView.swift
+++ DemoCustomViewCustomViewModifier20230223/ContentView.swift
@@ -10,14 +10,48 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Tap Me")
+                .modifier(ButtonLabel())
+            Image(systemName: "phone")
+                .modifier(ButtonLabel())
+            Spacer()
+                .frame(height: 40)
+            HStack {
+                Image(systemName: "apple.logo")
+                Text("Sign in with Apple")
+            }
+            .modifier(ButtonLabel(width: 210, height: 40, cornerRadius: 5, fgColor: .white, bgColor: .black))
+            VStack {
+                Text("Send a message")
+                Spacer()
+                    .frame(height: 10)
+                Image(systemName: "message.fill")
+            }
+            .modifier(ButtonLabel(width: 150, height: 70, cornerRadius: 10, fgColor: .white, bgColor: .pink))
         }
         .padding()
     }
 }
+
+
+
+struct ButtonLabel: ViewModifier {
+    var width: CGFloat = 150
+    var height: CGFloat = 40
+    var cornerRadius: CGFloat = 10
+    var fgColor: Color = .white
+    var bgColor: Color = .indigo
+    
+    func body(content: Content) -> some View {
+        content
+            .bold()
+            .frame(width: width, height: height)
+            .background(bgColor)
+            .cornerRadius(cornerRadius)
+            .foregroundColor(fgColor)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
