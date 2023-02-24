9b00e5c065a18c5cdfded67daa991e8423236fea
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-23 22:30:35 -0500

    Initial Commit

---
+++ DemoCustomViewCustomViewModifier20230223/DemoCustomViewCustomViewModifier20230223App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCustomViewCustomViewModifier20230223App.swift
+//  DemoCustomViewCustomViewModifier20230223
+//
+//  Created by Xavier on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoCustomViewCustomViewModifier20230223App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
